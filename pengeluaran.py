e54b10c48ae63719d58fa418c5831e77acb667a7
rzlnurfuadi <rizalnurfuadi@upi.edu>
2024-11-22 21:57:32 +0700

    memperbaiki sedikit kodingan dan menambah data_pengeluaran

--- pengeluaran.py
+++ pengeluaran.py
@@ -5,14 +5,14 @@ DATA_PENGELUARAN = "data/data_pengeluaran.json"
 
 def load_data():
     try:
-        with open(DATA_PENNGELUARAN, "r") as file:
-            return json>load(file)
+        with open(DATA_PENGELUARAN, "r") as file:
+            return json.load(file)
     except (FileNotFoundError, json.JSONdecodeError):
         return{}
     
 def save_data(data):
     with open(DATA_PENGELUARAN, "w") as file:
-        json.dump(data, file indent=4)
+        json.dump(data, file, indent=4)
         
 def generate_id(data):
     pengeluaran_id = f"PLRN{len(data) + 1}"
@@ -30,22 +30,22 @@ def add_pengeluaran():
     tanggal_pengeluaran =input("Tanggal Pengeluaran (YYYY-MM-DD): ")
     kategori_pengeluaran = input("MAsukkan kategori pengeluaran (Biaya pupuk atau Biaya tenaga kerja): ")
     
-try:
-    pengeluaran_operasional = float(pengeluaran_operasional)
-    datetime.strptime(tanggal_pengeluaran, "%Y-%m-%d")
-except ValueError:
-    print("Error: Input tidak valid. Pastikan pengeluaran operasional adlah angka dan tanggal pengeluaran dalam format yang benar! ")
-    return
+    try:
+        pengeluaran_operasional = float(pengeluaran_operasional)
+        datetime.strptime(tanggal_pengeluaran, "%Y-%m-%d")
+    except ValueError:
+        print("Error: Input tidak valid. Pastikan pengeluaran operasional adlah angka dan tanggal pengeluaran dalam format yang benar! ")
+        return
 
-data[id_pengeluaran] = {
-    "id": id_pengeluaran, 
-    "pengeluaran_operasional": pengeluaran_operasional,
-    "tanggal_pengeluaran": tanggal_pengeluaran,
-    "kategori_pengeluaran": kategori_pengeluaran
+    data[id_pengeluaran] = {
+        "id": id_pengeluaran, 
+        "pengeluaran_operasional": pengeluaran_operasional,
+        "tanggal_pengeluaran": tanggal_pengeluaran,
+        "kategori_pengeluaran": kategori_pengeluaran
 }
 
-save_data(data)
-print("Data pengeluaran berhasil ditambahkan!")
+    save_data(data)
+    print("Data pengeluaran berhasil ditambahkan!")
 
 def read_pengeluara():
     data = load_data()
